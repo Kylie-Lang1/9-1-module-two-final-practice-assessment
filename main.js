29dcb77e30e2b95c2860ae0c7af4c0ac91e3a428
Kylie-Lang1 <kylielang@pursuit.org>
2022-09-19 10:18:23 -0400

    Appends shoutouts to ul

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const age = document.createElement(`p`)
 const eyes = document.createElement(`p`)
 const hair = document.createElement(`p`)
 const options = document.querySelectorAll(`option`)
+const option = document.querySelector(`option`)
 const ul = document.querySelector(`ul`)
+const text = document.querySelector(`#shoutout`)
+const submit = document.querySelector(`#submit`)
+const form = document.querySelector(`form`)
+
+let isSelected = ``
 
 fetch (`${BASE_URL}/people`)
     .then (res => res.json())
@@ -20,19 +26,43 @@ fetch (`${BASE_URL}/people`)
 
             select.addEventListener(`change`, (event) => {
                 event.preventDefault()
-                console.log(event)
 
                 const index = event.path[0].selectedIndex
 
-                info.append(personName, age, eyes, hair)
-                personName.textContent = `Name: ${res[index-1][`name`]}`
-                age.textContent = `Age: ${res[index-1][`age`]}`
-                eyes.textContent = `Eyes: ${res[index-1][`eye_color`]}`
-                hair.textContent = `Hair Color: ${res[index-1][`hair_color`]}`
+                if(index > 0){
+                    info.append(personName, age, eyes, hair)
+                    personName.textContent = `Name: ${res[index-1][`name`]}`
+                    age.textContent = `Age: ${res[index-1][`age`]}`
+                    eyes.textContent = `Eyes: ${res[index-1][`eye_color`]}`
+                    hair.textContent = `Hair Color: ${res[index-1][`hair_color`]}`
+                    isSelected = `yes`
+                } else {
+                    isSelected = `no`
+                }
+                // console.log(isSelected)
+                if(isSelected === `no`){
+                    const alert = document.createElement(`p`)
+                    alert.textContent = `Please Select a Person`
+                    alert.classList.add(`alert`)
+                    select.after(alert)
+                }
             })
-        }
+        } 
 
+        console.log(isSelected)
         
+
+        form.addEventListener(`submit`, (event) => {
+            event.preventDefault()
+
+            const li = document.createElement(`li`)
+            li.innerHTML = 
+                `<p><strong>${personName.textContent.substring(6)}</strong>: ${event.target[0].value}</p>`
+            ul.append(li)
+
+            console.log(li.innerHTML)
+            form.reset()
+        })      
     })
     .catch (err => console.log(err))
 
